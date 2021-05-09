8ba30daafbad86d77d8433f0823c624e8b8913f5
ktodoran <ktodoran@gmail.com>
2021-05-09 18:44:38 -0400

    Corrected Timer & Added Questions w/ Display

---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+//Array of Questions//
+
+var quiz = [
+ {
+    q: 'What year did Disneyland open?',
+    options: ['1953, 1956, 1962, 1955'],
+    answer: 3
+ },
+ {
+    q: 'What is the name of the Toy Store in Toy Story 2?',
+    options: ['Monjo\'s Toys, Kids Wonders, Al\'s Toy Barn, Toys R\' Us'],
+    answer: 2
+ },
+ {
+    q: 'What are the names of Hades\' minions in Hercules',
+    options: ['Pain and Panic, Timon and Pumba, Mickey and Donald, Ren and Stimpy'],
+    answer: 0
+ },
+ {
+    q: 'Which is NOT the name of one of the seven dwarfs in Snow White?',
+    options: ['Doc, Sleepy, Happy, Angry'],
+    answer: 3
+ },
+ {
+    q: 'How long was Genie stuck in his lamp before Aladdin found him?',
+    options: ['5 minutes, 10,000 years, 150 years, 13 hours'],
+    answer: 1
+ }
+]
+
+console.log(quiz)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,53 @@
 //Countdown Timer//
-
 var totalSeconds = 75;
+
+function countdownTimer() {
+    totalSeconds = totalSeconds - 1;
+    document.getElementById("timeleft").textContent = totalSeconds;
+    if (totalSeconds <=0){
+        clearInterval(counter);
+}
+}
+
+//Show Rules List Function
+function showRules() {
+    var rulesList = document.getElementById("rulesdiv");
+        rulesList.style.display = "block";
+  }
+
+//Start Questions Function
+function startQuiz() {
+    var openQuestions = document.getElementById("que-text");
+    openQuestions.style.display = "block";
+    setInterval(countdownTimer, 1000);
+}
+
+function submitAnswer(question, correct) {
+    if (!correct) {
+        totalSeconds -= 10;
+        document.getElementById("timeleft").textContent = totalSeconds;
+    }
+    if (question === 1) {
+    document.getElementById("que_one").style.display = "none";
+    document.getElementById("que_two").style.display = "block";
+    }
+    else if (question === 2) {
+    document.getElementById("que_two").style.display = "none";
+    document.getElementById("que_three").style.display = "block";
+    }
+    else if (question === 3) {
+    document.getElementById("que_three").style.display = "none";
+    document.getElementById("que_four").style.display = "block";
+}
+    else if (question === 4){
+    document.getElementById("que_four").style.display = "none";
+    document.getElementById("que_five").style.display = "block";
+}
+    else if (question === 5){
+    document.getElementById("que_five").style.display = "none";
+    document.getElementById("").style.display = "block";
+}
+}
+
+
+
