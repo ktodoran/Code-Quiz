def909949bc19cde9c5c961b87fca64258c28e0d
ktodoran <ktodoran@gmail.com>
2021-05-10 00:04:17 -0400

    Created multiple sections for Highscores and styled questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,52 @@
 //Countdown Timer//
 var totalSeconds = 75;
+var countdownInterval;
 
 function countdownTimer() {
-    totalSeconds = totalSeconds - 1;
+    var temporarySeconds = totalSeconds -1;
+    if(temporarySeconds < 0) {
+        totalSeconds = 0;
+    }
+    else {
+        totalSeconds = temporarySeconds
+    }
     document.getElementById("timeleft").textContent = totalSeconds;
-    if (totalSeconds <=0){
-        clearInterval(counter);
+    if (totalSeconds === 0){
+        clearInterval(countdownInterval);
+        document.getElementById("que_five").style.display = "none";
+        submitAnswer(5);
 }
 }
-
 //Show Rules List Function
 function showRules() {
     var rulesList = document.getElementById("rulesdiv");
         rulesList.style.display = "block";
-  }
-
+}
 //Start Questions Function
 function startQuiz() {
-    var openQuestions = document.getElementById("que-text");
-    openQuestions.style.display = "block";
-    setInterval(countdownTimer, 1000);
+    document.getElementById("rulesdiv").style.display = "none";
+    document.getElementById("start-page").style.display = "none";
+    document.getElementById("que-text").style.display = "block";
+    countdownInterval = setInterval(countdownTimer, 1000);
+}
+
+function goBack() {
+    document.getElementById("rulesdiv").style.display = "none";
+    document.getElementById("que-text").style.display = "none";
+    document.getElementById("start-page").style.display = "block";
 }
 
 function submitAnswer(question, correct) {
     if (!correct) {
-        totalSeconds -= 10;
-        document.getElementById("timeleft").textContent = totalSeconds;
-    }
+        var temporarySeconds = totalSeconds - 10;
+        if (temporarySeconds < 0) {
+            totalSeconds = 0;
+        }
+        else {
+            totalSeconds = temporarySeconds;
+        }
+            document.getElementById("timeleft").textContent = totalSeconds;
+}
     if (question === 1) {
     document.getElementById("que_one").style.display = "none";
     document.getElementById("que_two").style.display = "block";
@@ -44,10 +64,38 @@ function submitAnswer(question, correct) {
     document.getElementById("que_five").style.display = "block";
 }
     else if (question === 5){
+    clearInterval(countdownInterval);
     document.getElementById("que_five").style.display = "none";
-    document.getElementById("").style.display = "block";
+    document.getElementById("sumbitscore").style.display = "block";
+    document.getElementById("score").textContent = 'Your Score is ' + totalSeconds;
+    
 }
 }
 
+function submitHighscore() {
+    var result = localStorage.getItem('highscores');
+    var highscores = [];
+    if (result) {
+        highscores = JSON.parse(result)
+    }
+    var initials = document.getElementById("initials").value;
+    var highscore = {initials, totalSeconds};
+    highscores.push(highscore);
+    localStorage.setItem('highscores', JSON.stringify(highscores));
+    document.getElementById("sumbitscore").style.display = "none"
+    viewHighscores();
+}
 
-
+function viewHighscores() {
+    document.getElementById("finalscores").style.display = "block"
+    document.getElementById("scorelist").innerHTML = '';
+    var result = localStorage.getItem('highscores');
+    var highscores = [];
+    if (result) {
+        highscores = JSON.parse(result)
+    }
+    highscores.sort((a,b) => (a.totalSeconds < b.totalSeconds)?1:-1);
+    for (let index = 0; index < highscores.length; index++) {
+        document.getElementById("scorelist").innerHTML += '<li>' + highscores[index].initials + "  " + highscores[index].totalSeconds  +   '<li>'
+    }
+}
